60df4c9935b7b32edaa59d2cf0df9a1f2b65ee73
Ted Vaala <ted.vaala@gmail.com>
2024-09-19 15:14:44 -0400

    Task1_2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -284,6 +284,8 @@ const handlePlantsClick = (e) => {
                 <div className="product-card" key={plantIndex}>
                     <img className="product-image" src={plant.image} alt={plant.name} />
                     <div className="product-title">{plant.name}</div>
+                    <div className="product-description">{plant.description}</div>
+                    <div className="product-cost">{plant.cost}</div>
                     {/*Similarly like the above plant.name show other details like description and cost*/}
                     <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                 </div>
